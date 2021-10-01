d6781ea741701c678ef2bdaba3e2d82a128190fa
mfbaltaza <araujogreydiego@gmail.com>
2021-09-30 21:08:19 -0400

    Removes unused component and refactor Feature component to rfce with interface

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -1,27 +1,18 @@
 import { Stack, Box, Heading, Text } from "@chakra-ui/layout"
 
-function Feature({ title, desc, ...rest }) {
-  return (
-    <Box p={5} shadow="md" m="2" borderWidth="1px" {...rest}>
-      <Heading fontSize="xl">{title}</Heading>
-      <Text mt={4}>{desc}</Text>
-    </Box>
-  )
+interface Props {
+  mt?: number,
+  mb?: number,
+  title: string,
+  desc: string,
 }
 
-function StackEx() {
+const Feature: React.FC<Props> = ({title, desc, ...rest}) => {
   return (
-    <Stack spacing={8}>
-      <Feature
-        title="Plan Money"
-        desc="The future can be even brighter but a goal without a plan is just a wish"
-      />
-      <Feature
-        title="Save Money"
-        desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings on your own terms with our completely automated process"
-      />
-    </Stack>
-  )
-}
+  <Box p={5} shadow="md" m="2" borderWidth="1px" {...rest}>
+    <Heading fontSize="xl">{title}</Heading>
+    <Text mt={4}>{desc}</Text>
+  </Box>
+)}
 
 export default Feature
